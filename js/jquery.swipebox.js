908468b1cc0618f34c4bd86f5ed7c427c78a0dbe
Daniel Sousa <dsousa.dev@gmail.com>
2014-07-16 18:25:20 +0100

    da
    da

--- js/jquery.swipebox.js
+++ js/jquery.swipebox.js
@@ -38,7 +38,7 @@
 						<a id="swipebox-close"></a>\
 						<a id="swipebox-prev"></a>\
 						<a id="swipebox-next"></a>\
-						<a style="color:white;  text-transform:uppercase; text-decoration:none;"><img src="images/star.png" style="width:24px; margin-bottom:-5px; margin-right:5px;">Adauga la favorite</a>
+						<a style="color:white;  text-transform:uppercase; text-decoration:none;"><img src="images/star.png" style="width:24px; margin-bottom:-5px; margin-right:5px;">Adauga la favorite</a> \
 					</div>\
 			</div>';
 
